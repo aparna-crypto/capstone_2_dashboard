84b0d7bef6d3e4aa1fe670636355e17a7e87cd94
aparna-crypto <80636725+aparna-crypto@users.noreply.github.com>
2022-12-03 12:42:07 +0530

    Create test_sample.py

---
+++ test_sample.py
@@ -0,0 +1,18 @@
+
+from untitled4 import *
+
+
+def test_add_num():
+    assert add(1, 2) == 3
+
+
+def test_add_str():
+    assert add("a", "b") == "ab"
+
+
+class TestSample:
+    def test_add_num(self):
+        assert add(1, 2) == 3
+
+    def test_add_str(self):
+        assert add("a", "b") == "ab"
